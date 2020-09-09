3a31bc9546f950c481b773f3d711e4cbf100e1b6
Lisa Buchholz <lb@integer-net.de>
2020-09-09 18:42:34 +0200

    add layered navigation, add breadcrumbs, add page title support for mobile

--- web/tailwind/postcss.config.js
+++ web/tailwind/postcss.config.js
@@ -2,6 +2,8 @@
     plugins: [
         require('postcss-import'),
         require('tailwindcss'),
+        require('postcss-nested'),
+        require('postcss-custom-properties'),
         require('autoprefixer'),
     ]
 }
